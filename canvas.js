2de19a40df0c289931a335d0a01c447aa34272db
Ricky <ricky_@live.ca>
2020-02-15 16:55:32 -0500

    updated with slider

--- canvas.js
+++ canvas.js
@@ -5,28 +5,38 @@ window.addEventListener("load", () => {
     canvas.height = window.innerHeight;
     canvas.weight = window.innerWidth;
 
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    context.lineWidth = 10;
+
+    slider.oninput = function () {
+        console.log(output.innerHTML);
+        output.innerHTML = this.value;
+        context.lineWidth = slider.value;
+        return output;
+    }
+
     let drawing = false;
 
-    function startingPointerPosition(e){
+    function startingPointerPosition(e) {
         drawing = true;
         pointerDraw(e);
     }
 
-    function startingTouchPosition(e){
+    function startingTouchPosition(e) {
         drawing = true;
         touchDraw(e);
     }
 
-    function endingPosition(){
+    function endingPosition() {
         drawing = false;
         context.beginPath();
     }
 
-    function pointerDraw(e){
-        if(!drawing){
+    function pointerDraw(e) {
+        if (!drawing) {
             return;
         }
-        //context.lineWidth = 10;
         context.lineCap = "round";
 
         context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
@@ -35,11 +45,11 @@ window.addEventListener("load", () => {
         context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
     }
 
-    function touchDraw(e){
-        if(!drawing){
+    function touchDraw(e) {
+        if (!drawing) {
             return;
         }
-        context.lineWidth = 10;
+
         context.lineCap = "round";
 
         context.lineTo(e.changedTouches[0].clientX - canvas.offsetLeft, e.changedTouches[0].clientY - canvas.offsetTop);
@@ -57,13 +67,9 @@ window.addEventListener("load", () => {
 });
 
 const clear = document.querySelector('#clear');
-clear.addEventListener('click', function(e) {
+clear.addEventListener('click', function (e) {
     e.preventDefault();
     console.log("Clear has been clicked");
     const context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
 });
-
-function penSize(e) {
-    context.lineWidth = 10;
-}
