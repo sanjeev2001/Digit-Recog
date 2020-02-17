1f2381eb6a6c7188a1ff9b344816101553d9c282
Sanjeev Sritharan <swaggermcpuff@gmail.com>
2020-02-17 02:58:39 -0500

    removed unnecessary files

--- script.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.querySelector("#yeet");
-const context = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerWidth;
-
-canvas.addEventListener("pointerout", function () {
-    console.log("pls");
-});
